d88434b30484862e6d5c848228831b07e524e0c8
Rahul <chrahulofficial@gmail.com>
2024-08-09 23:17:06 +0530

    dn

--- routes/Savepdf.js
+++ routes/Savepdf.js
@@ -5,17 +5,13 @@ const users = require("../models/User.js");
 SavepdfRoute.post("/:id", async (req, res) => {
   try {
     const { url } = req.body;
-
     const userId = req.params.id;
     const user = await users.findOne({ _id: userId });
     if (!user) {
       return res.status(404).send("User Not exists");
     }
-
     user.pdf = url;
-
     await user.save();
-
     res.status(200).json({ message: "PDF URL updated successfully" });
   } catch (error) {
     console.log(error);
